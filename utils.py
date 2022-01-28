dbf154f475380954d6a57a17b2fe92e276909771
Djamel Hemch <75702197+tekm06@users.noreply.github.com>
2022-01-28 21:52:27 +0100

    changeAcess script and updates on utils

--- utils.py
+++ utils.py
@@ -3,55 +3,47 @@ from statistics import mode
 import numpy as np
 from scipy.spatial import distance as dist
 import matplotlib.pyplot as plt
+from glob import glob
 import os
+from os.path import exists
 import pandas as pd
 import dataframe_image as dfi
 from datetime import datetime
-
 def eye_aspect_ratio(eye):
-    '''
-    This function computes the eye aspect ratio for the eye-blink anti-spoofing technique
-    '''
+    # compute the euclidean distances between the two sets of
+    # vertical eye landmarks (x, y)-coordinates
     A = dist.euclidean(eye[1], eye[5])
     B = dist.euclidean(eye[2], eye[4])
+    # compute the euclidean distance between the horizontal
+    # eye landmark (x, y)-coordinates
     C = dist.euclidean(eye[0], eye[3])
-    
+    # compute the eye aspect ratio
     ear = (A + B) / (2.0 * C)
-    
+    # return the eye aspect ratio
     return ear
-
 def findCosineDistance(source_representation, test_representation):
-    '''
-    This function calculates the  Cosine Distance between two feature vectors
-    '''
     a = np.matmul(np.transpose(source_representation), test_representation)
     b = np.sum(np.multiply(source_representation, source_representation))
     c = np.sum(np.multiply(test_representation, test_representation))
-
     return 1 - (a / (np.sqrt(b) * np.sqrt(c)))
 
 
-def prediction_cosine_similarity2(x_train, y_train, x_test, neighbors):
-    '''
-    This function classifies a new face based on the saved NPY files (x_train, y_train)
-    The neighbor input defines the n closest feature vector for a k-NN classification
-    '''
-    size = np.shape(x_train)[0]  
+def prediction_cosine_similarity2(x_train, y_train, x_test, neigbors):
+    size = np.shape(x_train)[0]  # nombre de vecteurs caracterestique dans le x_train
     distance = []
     sorted_distance = []
     predictions = []
     y_pred = 0
-
-    for i in range(0, size):  
+    for i in range(0, size):  # calcul distance entre x_train et le vecteur a classifier
         d = findCosineDistance(x_train[i], x_test[0])
         distance.append(d)
     sorted_distance = np.sort(distance)
-
+    print(sorted_distance[0])
     if sorted_distance[0] >= 0.2:
         y_pred = np.array(["Not Recognized"])
-
+        print(y_pred)
     else:
-        for j in range(0, neighbors):
+        for j in range(0, neigbors):
             indice = np.where(distance == sorted_distance[j])[0][0]
             predictions.append(y_train[indice])
             try:
@@ -59,12 +51,17 @@ def prediction_cosine_similarity2(x_train, y_train, x_test, neighbors):
             except:
                 indice_nomode = np.where(distance == sorted_distance[0])[0][0]
                 y_pred = y_train[indice_nomode]
+    print(y_pred)
     return y_pred
 
+
+
+
+
+
+
+
 def donutGenerator(path, output_filename):
-    '''
-    This function generates a donuts figures to diplay it in the dashboard view 
-    '''
     auth = np.load(path)
 
     data = np.unique(auth, return_counts=True)[1]
@@ -99,6 +96,8 @@ def generate_report(source_path, data_files, columns_titles, output_type, output
     data = np.array([np.load(source_path + data_files[0])])
     accepted_output_extensions = ["csv", "png"]
     for data_file in data_files[1:]:
+        print(np.shape(data))
+        print(np.shape(np.array([np.load(source_path + data_file)])))
         data = np.concatenate((data, np.array([np.load(source_path + data_file)])), axis=0)
     dataDf = pd.DataFrame(data=np.transpose(data), columns=columns_titles)
 
@@ -121,3 +120,94 @@ def generate_report(source_path, data_files, columns_titles, output_type, output
             accepted_output_extensions))
 
     return dataDf
+
+
+def delete_personel(model_name, class_name):
+    '''
+    This function deletes a selected personel from the gallery (x_train and y_train)
+    '''
+
+    if model_name == 'vgg16' or model_name == 'resnet50':
+        x_train = np.load('./gallery/' + model_name + '_x-train.npy')
+        y_train = np.load('./gallery/' + model_name + '_y-train.npy')
+    else:
+        print('This {} model is not recognized. The available ones are vgg16 or resnet 50'.format(model_name))
+        return None
+
+    if class_name in y_train:
+        indexes = np.where(y_train == class_name)[0]
+    else:
+        print('{} does not exist in the gallery'.format(class_name))
+        return None
+
+    if exists('./gallery/deleted_' + model_name + '_y-train.npy'):
+        deleted_x_train = np.load('./gallery/deleted_' + model_name + '_x-train.npy')
+        deleted_y_train = np.load('./gallery/deleted_' + model_name + '_y-train.npy')
+
+        deleted_x_train = np.concatenate((deleted_x_train, np.expand_dims(x_train[indexes[0]], axis=0)), axis=0)
+        deleted_y_train = np.concatenate((deleted_y_train, np.expand_dims(y_train[indexes[0]], axis=0)), axis=0)
+    else:
+        deleted_x_train = np.array([])
+        deleted_y_train = np.array([])
+
+        deleted_x_train = np.expand_dims(np.append(deleted_x_train, x_train[indexes[0]]), axis=0)
+        deleted_y_train = np.expand_dims(np.append(deleted_y_train, y_train[indexes[0]]), axis=0)
+
+    for index in indexes[1:]:
+        feature_to_delete = np.expand_dims(x_train[index], axis=0)
+        label_to_delete = np.expand_dims(y_train[index], axis=0)
+        deleted_x_train = np.concatenate((deleted_x_train, feature_to_delete), axis=0)
+        deleted_y_train = np.concatenate((deleted_y_train, label_to_delete), axis=0)
+
+    x_train = np.delete(x_train, indexes, axis=0)
+    y_train = np.delete(y_train, indexes, axis=0)
+
+    np.save('./gallery/deleted_' + model_name + '_x-train.npy', deleted_x_train)
+    np.save('./gallery/deleted_' + model_name + '_y-train.npy', deleted_y_train)
+
+    np.save('./gallery/' + model_name + '_x-train.npy', x_train)
+    np.save('./gallery/' + model_name + '_y-train.npy', y_train)
+
+    print("The {} {} samples have been deleted with success".format(class_name, model_name.upper()))
+
+    return None
+
+
+def reinteger_all(model_name):
+    '''
+    This function reintegrates all removed collaborators again to the gallery
+    '''
+
+    if model_name == 'vgg16' or model_name == 'resnet50':
+        if not exists('./gallery/deleted_' + model_name + '_y-train.npy'):
+            return print('Any collaborator has been removed previously from the gallery to reintegrate')
+        else:
+            deleted_x_train = np.load('./gallery/deleted_' + model_name + '_x-train.npy')
+            deleted_y_train = np.load('./gallery/deleted_' + model_name + '_y-train.npy')
+            if deleted_y_train.size == 0:
+                return print('Any collaborator has been removed previously from the gallery to reintegrate')
+    else:
+        print('This {} model is not recognized. The available ones are vgg16 or resnet 50'.format(model_name))
+        return None
+
+    x_train = np.load('./gallery/' + model_name + '_x-train.npy')
+    y_train = np.load('./gallery/' + model_name + '_y-train.npy')
+
+    x_train = np.concatenate((x_train, deleted_x_train), axis=0)
+    y_train = np.concatenate((y_train, deleted_y_train), axis=0)
+
+    np.save('./gallery/' + model_name + '_x-train.npy', x_train)
+    np.save('./gallery/' + model_name + '_y-train.npy', y_train)
+
+    print('==================================================================================')
+    print('All removed collaborators have been reintegrated to the gallery again with sucess')
+    print('The reintegrated collaborators are :{}'.format(np.unique(deleted_y_train, return_counts=True)[0]))
+    print('==================================================================================')
+
+    deleted_x_train = np.array([])
+    deleted_y_train = np.array([])
+    np.save('./gallery/deleted_' + model_name + '_x-train.npy', deleted_x_train)
+    np.save('./gallery/deleted_' + model_name + '_y-train.npy', deleted_y_train)
+
+    return None
+

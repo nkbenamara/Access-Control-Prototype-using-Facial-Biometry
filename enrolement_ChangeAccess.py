82fecf899cca241cfbd8358461e45befe37e679a
nkbenamara <nk.benamara@gmail.com>
2022-05-14 21:57:25 +0100

    all_personel_Grant_Access_Vue

--- enrolement_ChangeAccess.py
+++ enrolement_ChangeAccess.py
@@ -24,6 +24,8 @@ from keras_vggface import utils
 from utils import prediction_cosine_similarity2, findCosineDistance
 import dlib
 
+from functools import partial
+
 from paths import *
 
 class Ui_enrolement_ChangeAccess(object):
@@ -42,7 +44,7 @@ class Ui_enrolement_ChangeAccess(object):
 
         #Main Window
         enrolement_ChangeAccess.setObjectName("enrolement_ChangeAccess")
-        enrolement_ChangeAccess.setFixedSize(900, 250)
+        enrolement_ChangeAccess.setFixedSize(950, 650)
         enrolement_ChangeAccess.setStyleSheet("background-color: #1b1553;"
         "color: #ff4b3c;"
         "font-family: Play;"
@@ -53,148 +55,105 @@ class Ui_enrolement_ChangeAccess(object):
         #Central Widget
         self.centralwidget = QtWidgets.QWidget(enrolement_ChangeAccess)
         self.centralwidget.setObjectName("centralwidget")
-        # Create combobox and add items.
-
-
-        #Selected User Picture
-        self.face_pic = QtWidgets.QLabel(enrolement_ChangeAccess)
-        self.face_pic.setGeometry(15,40,170,170)
-        self.face_pic.setStyleSheet("border: 1px solid white;")
-        self.face_pic.setText("Selected \n User")
-        self.face_pic.setFont(font7)
-        self.face_pic.setAlignment(QtCore.Qt.AlignCenter)
-        self.face_pic.setScaledContents(True)
-
-        #Class List
-        self.comboBox = QComboBox(enrolement_ChangeAccess)
-        self.comboBox.setGeometry(568, 85, 200, 30)
-        self.comboBox.setObjectName(("comboBox"))
-        self.comboBox.addItem("Select user...")
-        self.comboBox.setStyleSheet(
-            "border-width: 2px;"
-            "border-radius: 10px;" 
-            "border-color: red;"
-            "background-color: #262626;"
-             "}"
-             
-        "QComboBox QAbstractItemView{"
-             "border: 2px;color:#BB86FC;"
-             "selection-background-color: #BB86FC;"
-             "}"
-        "QComboBox::item: selected"
-        "{"
-            "background-color:rgb(170, 170, 255);"
-            "color: #BB86FC;"
-        "}")
+        
+  
+        authorizations = np.load(HISTORY_PATH+'authorized.npy')
 
         options = load_actual_collaborators("vgg16")
+        user_number=1
+        #instantiate multiple label/buttons objects with a loop (TO SOLVE !!!!!!!!!!!!!!!)
         for option in options:
-            self.comboBox.addItem(option)
+            label_name= option+'_label'
+            remove_button_name=option+'_remove_button'
+            grant_button_name=option+'_grant_button'
+
+            
+            if user_number%5==0:
+                column = 4
+                row = (user_number//5) - 1 
+            else: 
+                column = user_number%5 -1
+                row = user_number//5
+
+            self.label_name = QtWidgets.QLabel(enrolement_ChangeAccess)
+            self.label_name.setGeometry(15 + column * 185,40 + row* 270,170,170)
+            self.label_name.setStyleSheet("border: 1px solid white;")
+            self.label_name.setText("Selected \n User {}".format(user_number))
+            self.label_name.setFont(font7)
+            self.label_name.setAlignment(QtCore.Qt.AlignCenter)
+            self.label_name.setScaledContents(True)
+            
+            path=glob(GALLERY_IMAGES_PATH+ option +"/*")[0]
+            face_img = cv2.imread(path)
+            height, width, channel = face_img.shape
+            step = channel * width
+            face_img = cv2.cvtColor(face_img, cv2.COLOR_BGR2RGB)
+            qImg = QImage(face_img.data, width, height, step, QImage.Format_RGB888)
+            self.label_name.setPixmap(QPixmap.fromImage(qImg))
+            
+            #Remove Access Button
+            self.remove_button_name = QtWidgets.QPushButton(enrolement_ChangeAccess)
+            self.remove_button_name.setGeometry(15 + column * 185, 260+ row* 250, 153, 40)
+            self.remove_button_name.setText("Remove Access")
+            self.remove_button_name.setIcon(QIcon("./imgs/delete.png"))
+            self.remove_button_name.setIconSize(QtCore.QSize(20, 20))
+            self.remove_button_name.setStyleSheet(
+            "QPushButton::hover"
+            "{"
+            "background-color:rgb(255,255,255);"
+            "color: black;"
+            "}"
+            "border: 1px solid white;"
+            )
+            self.remove_button_name.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
+            self.remove_button_name.setVisible(False)
+            self.remove_button_name.clicked.connect(partial(self.Remove, option))
+
+            #Grant Access Button
+            self.grant_button_name = QtWidgets.QPushButton(enrolement_ChangeAccess)
+            self.grant_button_name.setGeometry(15 + column * 185, 260 + row* 250, 153, 40)
+            self.grant_button_name.setText("Grant Access")
+            self.grant_button_name.setIcon(QIcon("./imgs/add.png"))
+            self.grant_button_name.setIconSize(QtCore.QSize(20, 20))
+            self.grant_button_name.setVisible(True)
+            self.grant_button_name.setStyleSheet(
+            "QPushButton::hover"
+            "{"
+            "background-color:rgb(255,255,255);"
+            "color: black;"
+            "}"
+            "border: 1px solid white;"
+            )
+            self.grant_button_name.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
+            self.grant_button_name.clicked.connect(partial(self.Grant, option))
+
+            if option in authorizations:
+
+                self.remove_button_name.setVisible(True)
+                self.grant_button_name.setVisible(False)
+            else: 
+                self.remove_button_name.setVisible(False)
+                self.grant_button_name.setVisible(True)
+
+            user_number+=1
+
+            
 
         #Authorization Status
         self.autorization_label = QtWidgets.QLabel(enrolement_ChangeAccess)
-        self.autorization_label.setGeometry(215, 80, 250, 50)
+        self.autorization_label.setGeometry(15, 215, 250, 50)
         self.autorization_label.setStyleSheet("font-size:20px;"
                                               "font-family: Play;")
 
-        #Edit Button                                      
-        self.editButton = QtWidgets.QPushButton(enrolement_ChangeAccess)
-        self.editButton.setGeometry(600, 150, 142, 40)
-        self.editButton.setText("Edit")
-        self.editButton.setIcon(QIcon("./imgs/edit.png"))
-        self.editButton.setIconSize(QtCore.QSize(30, 30))
-        self.editButton.setStyleSheet("QPushButton""{"
-                                "background-color: #262626"
-                                      "}"
-            "QPushButton::hover"
-            "{"
-            "background-color:rgb(255,255,255);"
-            "color: black;"
-            "}"
-            "border: 1px solid white;"
-        )
-        self.editButton.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
-        self.editButton.clicked.connect(self.editPersonnel)
 
-        #Remove Access Button
-        self.remove_access = QtWidgets.QPushButton(enrolement_ChangeAccess)
-        self.remove_access.setGeometry(215, 120, 153, 40)
-        self.remove_access.setText("Remove Access")
-        self.remove_access.setIcon(QIcon("./imgs/delete.png"))
-        self.remove_access.setIconSize(QtCore.QSize(20, 20))
-        self.remove_access.setStyleSheet(
-            "QPushButton::hover"
-            "{"
-            "background-color:rgb(255,255,255);"
-            "color: black;"
-            "}"
-            "border: 1px solid white;"
-        )
-        self.remove_access.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
-        self.remove_access.setVisible(False)
-        self.remove_access.clicked.connect(self.Remove)
 
-        #Grant Access Button
-        self.grant_access = QtWidgets.QPushButton(enrolement_ChangeAccess)
-        self.grant_access.setGeometry(215, 120, 153, 40)
-        self.grant_access.setText("Grant Access")
-        self.grant_access.setIcon(QIcon("./imgs/add.png"))
-        self.grant_access.setIconSize(QtCore.QSize(20, 20))
-        self.grant_access.setVisible(False)
-        self.grant_access.setStyleSheet(
-            "QPushButton::hover"
-            "{"
-            "background-color:rgb(255,255,255);"
-            "color: black;"
-            "}"
-            "border: 1px solid white;"
-        )
-        self.grant_access.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
-        self.grant_access.clicked.connect(self.Grant)
 
+    def Grant(self, person):
+        print(person)
         
-
-    def editPersonnel(self):
-        person = self.comboBox.currentText()
-        path=glob(MAIN_PATH + "/dataset_cropped/"+ person +"/*")[0]
+        remove_button=person+'_remove_button'
+        grant_button=person+'_grant_button'
         
-        print(path)
-        face_img = cv2.imread(path)
-        height, width, channel = face_img.shape
-        step = channel * width
-        y_train = np.load(GALLERY_PATH+'vgg16_y_train.npy')
-        authorizations = np.load(HISTORY_PATH+'authorized.npy')
-        if os.path.isdir(path) == False:
-            print("user not selected")
-            self.autorization_label.setText("Please select a user!")
-        if person in y_train:
-            if person in authorizations:
-                self.autorization_label.setText("{} is authorized".format(person))
-                self.autorization_label.setStyleSheet("color: #ff4b3c;"
-                                                      "font-size: 18px;")
-                self.remove_access.setVisible(True)
-                self.grant_access.setVisible(False)
-            else:
-                self.autorization_label.setText("{} is unauthorized".format(person))
-                self.autorization_label.setStyleSheet("color: #ff4b3c;"
-                                                      "font-size: 18px;")
-                self.autorization_label.setAlignment(QtCore.Qt.AlignLeft)
-                self.remove_access.setVisible(False)
-                self.grant_access.setVisible(True)
-
-        else:
-            self.autorization_label.setText("{} is not part of the personnel".format(person))
-            self.autorization_label.setStyleSheet("color: #FFFFFF;"
-                                                  "font-size: 18px")
-            self.remove_access.setVisible(False)
-            self.grant_access.setVisible(False)
-
-        face_img = cv2.cvtColor(face_img, cv2.COLOR_BGR2RGB)
-        qImg = QImage(face_img.data, width, height, step, QImage.Format_RGB888)
-        self.face_pic.setPixmap(QPixmap.fromImage(qImg))
-
-    def Grant(self):
-        person = self.comboBox.currentText()
         authorizations = np.load(HISTORY_PATH+'authorized.npy')
         authorizations = np.append(authorizations, str(person))
         print(authorizations)
@@ -202,12 +161,15 @@ class Ui_enrolement_ChangeAccess(object):
         self.autorization_label.setText("{} is now authorized".format(person))
         self.autorization_label.setStyleSheet("color: #ff4b3c;"
                                               "font-size: 18px;")
-        self.remove_access.setVisible(True)
-        self.grant_access.setVisible(False)
+        #self.remove_button.setVisible(True)
+        #self.grant_button.setVisible(False)
 
 
-    def Remove(self):
-        person = self.comboBox.currentText()
+    def Remove(self, person):
+        print(person)
+        remove_button=person+'_remove_button'
+        grant_button=person+'_grant_button'
+
         authorizations = np.load(HISTORY_PATH+'authorized.npy')
         index = np.where(authorizations == person)[0]
         print(index)
@@ -219,8 +181,8 @@ class Ui_enrolement_ChangeAccess(object):
                                                   "font-size: 18px;"
 )
             print(authorizations)
-        self.remove_access.setVisible(False)
-        self.grant_access.setVisible(True)
+        #self.remove_button.setVisible(False)
+        #self.grant_button.setVisible(True)
 
     def retranslateUi(self, enrolement_ChangeAccess):
         _translate = QtCore.QCoreApplication.translate

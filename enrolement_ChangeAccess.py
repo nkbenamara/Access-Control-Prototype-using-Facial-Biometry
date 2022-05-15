46195ed29a1fbbeb53b7b377e70cc245ccd6610d
nkbenamara <nk.benamara@gmail.com>
2022-05-15 21:03:08 +0100

    change_access_multi_objects_creation_fix

--- enrolement_ChangeAccess.py
+++ enrolement_ChangeAccess.py
@@ -62,11 +62,14 @@ class Ui_enrolement_ChangeAccess(object):
         options = load_actual_collaborators("vgg16")
         user_number=1
         #instantiate multiple label/buttons objects with a loop (TO SOLVE !!!!!!!!!!!!!!!)
-        for option in options:
-            label_name= option+'_label'
-            remove_button_name=option+'_remove_button'
-            grant_button_name=option+'_grant_button'
 
+        self.label_name_objects=[]
+        self.authorization_label_name_objects=[]
+        self.remove_button_name_objects=[]
+        self.grant_button_name_objects=[]
+
+
+        for option in options:
             
             if user_number%5==0:
                 column = 4
@@ -75,10 +78,10 @@ class Ui_enrolement_ChangeAccess(object):
                 column = user_number%5 -1
                 row = user_number//5
 
+            #User Label
             self.label_name = QtWidgets.QLabel(enrolement_ChangeAccess)
             self.label_name.setGeometry(15 + column * 185,40 + row* 270,170,170)
             self.label_name.setStyleSheet("border: 1px solid white;")
-            self.label_name.setText("Selected \n User {}".format(user_number))
             self.label_name.setFont(font7)
             self.label_name.setAlignment(QtCore.Qt.AlignCenter)
             self.label_name.setScaledContents(True)
@@ -91,9 +94,19 @@ class Ui_enrolement_ChangeAccess(object):
             qImg = QImage(face_img.data, width, height, step, QImage.Format_RGB888)
             self.label_name.setPixmap(QPixmap.fromImage(qImg))
             
+            self.label_name_objects.append(self.label_name)
+            #Authorization Status
+            self.authorization_label_name = QtWidgets.QLabel(enrolement_ChangeAccess)
+            self.authorization_label_name.setGeometry(15 + column * 185, 215 + row* 280, 170, 40)
+            self.authorization_label_name.setStyleSheet("font-size:16px;"
+                                              "font-family: Play;")
+            self.authorization_label_name.setAlignment(QtCore.Qt.AlignCenter)
+            
+            
+            self.authorization_label_name_objects.append(self.authorization_label_name)
             #Remove Access Button
             self.remove_button_name = QtWidgets.QPushButton(enrolement_ChangeAccess)
-            self.remove_button_name.setGeometry(15 + column * 185, 260+ row* 250, 153, 40)
+            self.remove_button_name.setGeometry(23 + column * 185, 260+ row* 280, 153, 40)
             self.remove_button_name.setText("Remove Access")
             self.remove_button_name.setIcon(QIcon("./imgs/delete.png"))
             self.remove_button_name.setIconSize(QtCore.QSize(20, 20))
@@ -107,11 +120,12 @@ class Ui_enrolement_ChangeAccess(object):
             )
             self.remove_button_name.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
             self.remove_button_name.setVisible(False)
-            self.remove_button_name.clicked.connect(partial(self.Remove, option))
+            self.remove_button_name.clicked.connect(partial(self.Remove,option,  user_number-1))
 
+            
             #Grant Access Button
             self.grant_button_name = QtWidgets.QPushButton(enrolement_ChangeAccess)
-            self.grant_button_name.setGeometry(15 + column * 185, 260 + row* 250, 153, 40)
+            self.grant_button_name.setGeometry(23 + column * 185, 260 + row* 280, 153, 40)
             self.grant_button_name.setText("Grant Access")
             self.grant_button_name.setIcon(QIcon("./imgs/add.png"))
             self.grant_button_name.setIconSize(QtCore.QSize(20, 20))
@@ -125,50 +139,43 @@ class Ui_enrolement_ChangeAccess(object):
             "border: 1px solid white;"
             )
             self.grant_button_name.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
-            self.grant_button_name.clicked.connect(partial(self.Grant, option))
+            self.grant_button_name.clicked.connect(partial(self.Grant,option,  user_number-1))
 
             if option in authorizations:
-
+                self.authorization_label_name.setText(option + '\n Authorized')
                 self.remove_button_name.setVisible(True)
                 self.grant_button_name.setVisible(False)
             else: 
+                self.authorization_label_name.setText(option + '\n Unauthorized')
                 self.remove_button_name.setVisible(False)
                 self.grant_button_name.setVisible(True)
+            
+            self.remove_button_name_objects.append(self.remove_button_name)
+            self.grant_button_name_objects.append(self.grant_button_name)
 
             user_number+=1
 
-            
+        print(self.label_name_objects)
+        print(self.authorization_label_name_objects)
+        print(self.remove_button_name_objects)
+        print(self.grant_button_name_objects)
 
-        #Authorization Status
-        self.autorization_label = QtWidgets.QLabel(enrolement_ChangeAccess)
-        self.autorization_label.setGeometry(15, 215, 250, 50)
-        self.autorization_label.setStyleSheet("font-size:20px;"
-                                              "font-family: Play;")
-
-
-
-
-    def Grant(self, person):
-        print(person)
-        
-        remove_button=person+'_remove_button'
-        grant_button=person+'_grant_button'
-        
+    def Grant(self,person, user_number):
+        print(user_number)
+ 
         authorizations = np.load(HISTORY_PATH+'authorized.npy')
         authorizations = np.append(authorizations, str(person))
         print(authorizations)
         np.save(HISTORY_PATH+'authorized.npy', authorizations)
-        self.autorization_label.setText("{} is now authorized".format(person))
-        self.autorization_label.setStyleSheet("color: #ff4b3c;"
-                                              "font-size: 18px;")
-        #self.remove_button.setVisible(True)
-        #self.grant_button.setVisible(False)
+        self.authorization_label_name_objects[user_number].setText("{}\n Authorized".format(person))
+        self.authorization_label_name_objects[user_number].setStyleSheet("color: #ff4b3c;"
+                                              "font-size: 16px;")
+        self.remove_button_name_objects[user_number].setVisible(True)
+        self.grant_button_name_objects[user_number].setVisible(False)
 
 
-    def Remove(self, person):
-        print(person)
-        remove_button=person+'_remove_button'
-        grant_button=person+'_grant_button'
+    def Remove(self, person, user_number):
+        print(user_number)
 
         authorizations = np.load(HISTORY_PATH+'authorized.npy')
         index = np.where(authorizations == person)[0]
@@ -176,13 +183,13 @@ class Ui_enrolement_ChangeAccess(object):
         if person in authorizations:
             authorizations = np.delete(authorizations, index)
             np.save(HISTORY_PATH+'authorized.npy', authorizations)
-            self.autorization_label.setText("{} is now unauthorized ".format(person))
-            self.autorization_label.setStyleSheet("color: #ff4b3c;"
-                                                  "font-size: 18px;"
+            self.authorization_label_name_objects[user_number].setText("{}\n Unauthorized ".format(person))
+            self.authorization_label_name_objects[user_number].setStyleSheet("color: #ff4b3c;"
+                                                  "font-size: 16px;"
 )
             print(authorizations)
-        #self.remove_button.setVisible(False)
-        #self.grant_button.setVisible(True)
+        self.remove_button_name_objects[user_number].setVisible(False)
+        self.grant_button_name_objects[user_number].setVisible(True)
 
     def retranslateUi(self, enrolement_ChangeAccess):
         _translate = QtCore.QCoreApplication.translate
